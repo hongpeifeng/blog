1a8c7ed952f71de58ced09e8ebb098476eccb4a8
洪培峰 <hongpeifeng@hongpeiengdeMBP.lan>
2021-03-10 00:13:06 +0800

    sh init v2

--- test/config2asset.dart
+++ test/config2asset.dart
@@ -9,11 +9,19 @@ import 'package:blog/model/model.dart';
 
 void main() {
 
+  final imgsPath = 'assets/imgs';
+  final markDownsPath = 'assets/markdowns';
+
+
+  String getSummery(String article) {
+    return '';
+  }
+
   test('生成配置', () {
     // 遍历文件夹，生成文章类型, 文章中的icon.svg, 为其图标
     print('${Directory.current}');
     List<ArticleGroupModel> groups = [];
-    List<ArticleGroupModel> articles = [];
+    List<ArticleModel> articles = [];
     final configDir = Directory('${Directory.current.path}/config');
     final dirs = configDir.listSync()
         .skipWhile((value) => path.basename(value.path).startsWith('.')) // 过滤隐藏文件
@@ -30,9 +38,42 @@ void main() {
       final markDowns = markDownDirectory.listSync()
           .skipWhile((value) => !path.basename(value.path).endsWith('.md')) // 过滤隐藏文件
           .toList();
+
+      /// 创建文件夹
+      final destMarkDownDirectory = Directory('$markDownsPath/${group.name}');
+      if (!destMarkDownDirectory.existsSync())
+        destMarkDownDirectory.create();
+
+      /// 遍历该目录下
       for (final FileSystemEntity markDown in markDowns) {
-        print(markDown.path);
+
+        final file = File(markDown.path);
+        final contents = file.readAsStringSync().split('----------');
+        final articleName = path.basename(markDown.path).substring(0, path.basename(markDown.path).indexOf('.'));
+        final articleAddress = '$markDownsPath/${group.name}/$articleName.md';
+
+        /// 拷贝markdown
+        final article = File(articleAddress);
+        article.create();
+        article.writeAsString(contents.last.trim());
+
+        /// 拷贝img
+        final imgfile = File('${markDown.path.substring(0, markDown.path.indexOf('.'))}');
+
+        final model = ArticleModel(
+          articleName: articleName,
+          createTime: contents.first.trim(),
+          lastModifiedTime: contents.first.trim(),
+          tag: group.name,
+          summary: getSummery(contents.last),
+          imageAddress: '$imgsPath/${group.name}/$articleName.svg',
+          articleAddress: '$markDownsPath/${group.name}/$articleName.md',
+        );
+
+        articles.add(model);
       }
+
+
     }
 
 //    for (final ArticleGroupModel group in groups) {
